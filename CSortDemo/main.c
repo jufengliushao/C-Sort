2ec8f158430a58a2504ab1509f70c170fba41e02
liushaofeng <jufengliushao@163.com>
2018-10-02 13:25:52 +0800

    create project

---
+++ CSortDemo/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  CSortDemo
+//
+//  Created by shaofeng liu on 2018/10/2.
+//  Copyright © 2018年 shaofeng liu. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
