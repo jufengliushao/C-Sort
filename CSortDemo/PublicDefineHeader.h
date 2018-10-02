9899006e51908852e43efbb453ae9e1335e7f5d7
liushaofeng <jufengliushao@163.com>
2018-10-02 13:40:46 +0800

    20181002 13:40
    创建全局文件

---
+++ CSortDemo/PublicDefineHeader.h
@@ -0,0 +1,20 @@
+//
+//  PublicDefineHeader.h
+//  CSortDemo
+//
+//  Created by shaofeng liu on 2018/10/2.
+//  Copyright © 2018年 shaofeng liu. All rights reserved.
+//
+
+#ifndef PublicDefineHeader_h
+#define PublicDefineHeader_h
+
+#define RAM_MEMORY 50  // 单位兆 内存容量
+#define SIZE_GMKB 1000 // 转换率 gb - > mb -> kb -> b
+#define LINE_LENGTH 101 // 每行的字节数
+#define BLOCK_MEMORY 4 // block 内存大小 单位为kb
+#define SOURCE_DATA_MEMORY 1 // gb 源文件大小
+#define FILE_PRIFX "/Users/shaofengliu/Documents/github/C-Sort/CSortDemo/" // 文件前缀
+#define FILE_SOURCE_NAME "source.txt" // 源文件名字
+
+#endif /* PublicDefineHeader_h */
